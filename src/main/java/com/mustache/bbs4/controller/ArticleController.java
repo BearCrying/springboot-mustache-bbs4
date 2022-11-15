850d99be839fe8ef1661ab8c74073277ba84815b
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-11-15 23:23:55 +0900

    [add] error페이지 생성, ArticleController에 showSingle() 작성
    showSingle()은 해당 id로 접속했을때 해당 제목과 컨텐츠를 보여줌
    show.mustache로 페이지 구현

--- src/main/java/com/mustache/bbs4/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs4/controller/ArticleController.java
@@ -5,9 +5,13 @@ import com.mustache.bbs4.domain.dto.ArticleDto;
 import com.mustache.bbs4.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -25,9 +29,19 @@ public class ArticleController {
         return "new";
     }
 
+    @GetMapping("/{id}")
+    public String showSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+        if (!optionalArticle.isEmpty()) {
+            model.addAttribute("article", optionalArticle.get());
+            return "show";
+        } else return "error";
+
+    }
     @PostMapping("")
     public String createArticle(ArticleDto articleDto) { //dto란 데이터 전송을 위한 오브젝트 Entity와는 구분해서 사용하는게좋음
         Article savedArticle = articleRepository.save(articleDto.toEntity());
-        return String.format("id:%d", savedArticle.getId());
+        return String.format("redirect:/articles/%d", savedArticle.getId());
     }
+
 }
