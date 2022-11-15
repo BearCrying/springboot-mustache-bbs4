cf132f9bbb54fb4f659be10d4835088a1e435bf2
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-11-15 22:59:10 +0900

    [add] ArticleDto 생성, ArticleController에 createArticle()작성
    사이트에서 입력받은 값이 ArticleDto를 통해 Entity로 만들어지고
    이를 Repository를 통해 DB에 저장함

--- src/main/java/com/mustache/bbs4/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs4/controller/ArticleController.java
@@ -1,15 +1,33 @@
 package com.mustache.bbs4.controller;
 
+import com.mustache.bbs4.domain.Article;
+import com.mustache.bbs4.domain.dto.ArticleDto;
+import com.mustache.bbs4.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 @RequestMapping("/articles")
 public class ArticleController {
+
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
 
     @GetMapping("/new")
     public String createArticlePage() {
         return "new";
+    }
+
+    @PostMapping("")
+    public String createArticle(ArticleDto articleDto) { //dto란 데이터 전송을 위한 오브젝트 Entity와는 구분해서 사용하는게좋음
+        Article savedArticle = articleRepository.save(articleDto.toEntity());
+        return String.format("id:%d", savedArticle.getId());
     }
 }
